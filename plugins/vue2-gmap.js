f4de519b41e875fb10904c79c0bf1497af8ca4cb
Frank-Altwein <f.altwein@tdsoftware.de>
2020-08-13 11:07:01 +0200

    initial commit

--- plugins/vue2-gmap.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue"
-import * as VueGoogleMaps from "vue2-google-maps"
-
-Vue.use(VueGoogleMaps, {
-   installComponents: true,
-   load: {
-      key: 'AIzaSyAB8ReY4Tf3nPMpuVeiQAShPHb45kwsEwo'
-   }
-});
