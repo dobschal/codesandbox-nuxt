760cab20468ab083a5a254c9e5c917d9863f6ad4
Bastian Kemmer <b.kemmer@tdsoftware.de>
2020-09-11 14:46:35 +0200

    material icons didnt work

--- plugins/vue-toasted.js
+++ plugins/vue-toasted.js
@@ -2,10 +2,6 @@ import Vue from "vue"
 import Toasted from "vue-toasted"
 
 Vue.use(Toasted, {
-   iconPack: "material",
    theme: "toasted-primary",
-   duration: 3000,
-   icon: {
-      name: "warning"
-   }
+   duration: 3000
 });
